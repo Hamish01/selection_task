99cc8ab674b32f672a607caec828bcdacb7a0c84
Hamish01 <79813180+Hamish01@users.noreply.github.com>
2021-03-18 16:07:10 +1100

    set up

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace selection_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
